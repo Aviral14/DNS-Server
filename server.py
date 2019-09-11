9bae2023999e91d37391634f450feed9944dc0c0
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-11 20:25:18 +0530

    Add feature to fetch ip from another dns server

--- server.py
+++ server.py
@@ -1,18 +1,18 @@
 import socketserver
 import validators
-from dnslib import DNSRecord, DNSHeader, DNSQuestion, RR, A
+from dnslib import DNSRecord, DNSHeader, DNSQuestion, RR, A, QTYPE
 import tld
+from dns import resolver
 from settings import *
 from host_file import *
 
 
 class DomainResolve(socketserver.BaseRequestHandler):
     def handle(self):
-        data = self.request[0]
+        data, socket = self.request
         d = DNSRecord.parse(data)
-        ip, domain = self.resolve_domain(query=f"{d.q.qname}")
+        ip, domain = self.resolve_domain(query=str(d.q.qname))
         response = self.generate_response(ip=ip, domain=domain, q_id=d.header.id)
-        socket = self.request[1]
         socket.sendto(response.pack(), self.client_address)
 
     def resolve_domain(self, query):
@@ -32,22 +32,33 @@ class DomainResolve(socketserver.BaseRequestHandler):
         try:
             return records[top_domain][domain][subdomain], query
         except KeyError:
-            return "", domain
+            resolve = resolver.Resolver()
+            resolve.nameservers = ["8.8.8.8"]
+            ips = resolve.query(query)
+            return ips, query
 
     def direct(self):
         pass
 
     def generate_response(self, ip, domain, q_id):
-        if not ip:
-            return DNSRecord(
+        if type(ip) == str:
+            a = DNSRecord(
                 DNSHeader(id=q_id, qr=1, aa=1, ra=1),
                 q=DNSQuestion(domain),
                 a=RR(domain, rdata=A(ip)),
             )
+            print(str(a))
+            return a
         else:
-            return DNSRecord(
-                DNSHeader(id=q_id, qr=0, aa=1, ra=3), q=DNSQuestion(domain)
+            record = DNSRecord(
+                DNSHeader(id=q_id, qr=1, aa=1, ra=1),
+                q=DNSQuestion(domain),
+                a=RR(domain, rdata=A(ip[0].address)),
             )
+            print(str(record))
+            for ip_addr in ip[1:]:
+                record.add_answer(RR(domain, QTYPE.A, rdata=A(ip_addr.address)))
+            return record
 
 
 dns_server = socketserver.UDPServer((HOST, PORT), DomainResolve)
