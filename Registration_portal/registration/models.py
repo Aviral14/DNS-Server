f33226d07148c37fb880466caaad715a5ad376f6
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-12 17:17:09 +0530

    Create Domain Database

--- Registration_portal/registration/models.py
+++ Registration_portal/registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+
+class Domain(models.Model):
+    User = get_user_model()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="registerer")
+    url = models.URLField(null=False, blank=False)
+    ip = models.IPAddressField(null=False, blank=False)
+    registered_at = models.DateTimeField(auto_now_add=True)
+
