e9b08d6ed8afc327c2c3ed331667259e5b31ca13
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-13 15:11:34 +0530

    Change Field name

--- Registration_portal/registration/models.py
+++ Registration_portal/registration/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth import get_user_model
 class Domain(models.Model):
     User = get_user_model()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="registerer")
-    url = models.URLField(null=False, blank=False)
+    domain = models.TextField(null=False, blank=False)
     ip = models.IPAddressField(null=False, blank=False)
     registered_at = models.DateTimeField(auto_now_add=True)
 
