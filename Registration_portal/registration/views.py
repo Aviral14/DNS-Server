708b171ee8d70669ffea3b4f624077bfb5b8de96
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-13 15:48:28 +0530

    Add update records function

--- Registration_portal/registration/views.py
+++ Registration_portal/registration/views.py
@@ -10,18 +10,45 @@ def register_domain(request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form_obj = form.save(commit=False)
+            if (
+                form_obj.domain[:7] == "http://"
+                or form_obj.domain[:8] == "https://"
+                or form_obj.domain[:4] == "www."
+            ):
+                messages.error(
+                    request, "Please Enter the domain in the format specified"
+                )
+                return redirect("")
             form_obj.user = request.user
-            status = check_duplicacy(url=form_obj.url, ip=form_obj.ip)
-            if status:
+            duplicacy = update_records(domain=form_obj.domain, ip=form_obj.ip)
+            if not duplicacy:
                 form_obj.save()
                 messages.success(request, "Domain Successfully Registered")
             else:
                 messages.error(request, "Given IP Address or Domain already exists")
-            return redirect("http://localhost:8000/register/")
+            return redirect("register-page")
+    else:
+        form = RegistrationForm()
+        return render(request, "register-page", {"form": form})
+
+
+def update_records(domain, ip):
+    domain_modified = "http://" + domain
+    domain_obj = tld.get_tld(domain_modified, as_object=True)
+    second_domain = domain_obj.domain
+    top_domain = domain_obj.tld
+    subdomain = domain_obj.subdomain
+    if records.get(top_domain, ""):
+        if second_domain in list(records[top_domain].keys()):
+            return True
+        elif subdomain in list(records[top_domain][second_domain].keys()):
+            return True
+        elif ip == records[top_domain][second_domain][subdomain]:
+            return True
         else:
-            form = RegistrationForm()
-            return render(request, "registeration/register.html", {"form": form})
+            records[top_domain].update({second_domain: {subdomain: ip}})
+            return False
+    else:
+        records.update({top_domain: {second_domain: {subdomain: ip}}})
+        return False
 
-
-def check_duplicacy(url, ip):
-    pass
