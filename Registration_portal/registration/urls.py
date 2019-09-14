b402bdfc6beffec4a7807f1a2b78438da12a76a0
Aviral Agarwal <aviralagarwal25@gmail.com>
2019-09-14 21:04:39 +0530

    Change file format to json

--- Registration_portal/registration/urls.py
+++ Registration_portal/registration/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.register_domain, name="register-page")]
+urlpatterns = [
+    path("", views.register_domain, name="register-page"),
+    path("signup/", views.signup, name="account_signup"),
+]
 
--- Registration_portal/registration/views.py
+++ Registration_portal/registration/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 import tld
+import json
 from .forms import RegistrationForm
-from .host_file import records
 
 
+@login_required
 def register_domain(request):
     if request.method == "POST":
         form = RegistrationForm(request.POST)
@@ -18,37 +20,58 @@ def register_domain(request):
                 messages.error(
                     request, "Please Enter the domain in the format specified"
                 )
-                return redirect("")
+                return redirect("register-page")
             form_obj.user = request.user
-            duplicacy = update_records(domain=form_obj.domain, ip=form_obj.ip)
+            duplicacy, records = update_records(domain=form_obj.domain, ip=form_obj.ip)
             if not duplicacy:
                 form_obj.save()
+                with open("registration/host_file.json", "w") as host_file:
+                    json.dump(records, host_file)
                 messages.success(request, "Domain Successfully Registered")
             else:
-                messages.error(request, "Given IP Address or Domain already exists")
+                if records == "Error":
+                    messages.error(request, "Enter a valid Domain Name!")
+                else:
+                    messages.error(request, "Given Domain Name already exists")
             return redirect("register-page")
+        else:
+            messages.error(
+                request, "Please Enter all the information in the correct format!"
+            )
+            return redirect("register-page")
+
     else:
         form = RegistrationForm()
-        return render(request, "register-page", {"form": form})
+        return render(request, "registration/register-page.html", {"form": form})
 
 
 def update_records(domain, ip):
+    records = json.loads(open("registration/host_file.json").read())
     domain_modified = "http://" + domain
-    domain_obj = tld.get_tld(domain_modified, as_object=True)
-    second_domain = domain_obj.domain
-    top_domain = domain_obj.tld
-    subdomain = domain_obj.subdomain
+    try:
+        domain_obj = tld.get_tld(domain_modified, as_object=True)
+        second_domain = domain_obj.domain
+        top_domain = domain_obj.tld
+        subdomain = domain_obj.subdomain
+    except:
+        return True, "Error"
+
     if records.get(top_domain, ""):
         if second_domain in list(records[top_domain].keys()):
-            return True
-        elif subdomain in list(records[top_domain][second_domain].keys()):
-            return True
-        elif ip == records[top_domain][second_domain][subdomain]:
-            return True
+            if subdomain in list(records[top_domain][second_domain].keys()):
+                return True, "Duplicate"
+            else:
+                records[top_domain].update({second_domain: {subdomain: ip}})
+                return False, records
+
         else:
             records[top_domain].update({second_domain: {subdomain: ip}})
-            return False
+            return False, records
     else:
         records.update({top_domain: {second_domain: {subdomain: ip}}})
-        return False
+        return False, records
+
+
+def signup(request):
+    return redirect("account-login")
 
